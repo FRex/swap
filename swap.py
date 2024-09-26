61278f2db45e3b0ce1f7dfe5a4cfd428e3709fb4
FRex <blazej.roszkowski@gmail.com>
2024-09-26 06:50:17 +0200

    streamline the code a bit by adding comments and breaking error handling out the main function

--- swap.py
+++ swap.py
@@ -15,6 +15,17 @@ def makename(extraitems):
     return hashlib.sha512(str(items).encode("UTF-8")).hexdigest()[:60] + ".tmp"
 
 
+def exit2onmissing(fnames):
+    """Print all missing files from fnames list and do sys.exit(2) if any are missing."""
+    missing = False
+    for fname in fnames:
+        if not os.path.exists(fname):
+            missing = True
+            print(f"{fname} does not exist", file=sys.stderr)
+    if missing:
+        sys.exit(2)
+
+
 def main():
     # TODO: cmdline syntax to swap entire groups not just 2 files
     if len(sys.argv) != 3:
@@ -22,26 +33,15 @@ def main():
         sys.exit(1)
 
     fname1, fname2 = sys.argv[1], sys.argv[2]
-    okay = True
-    if not os.path.exists(fname1):
-        print(f"{fname1} does not exist", file=sys.stderr)
-        okay = False
-
-    if not os.path.exists(fname2):
-        print(f"{fname2} does not exist", file=sys.stderr)
-        okay = False
-
-    if not okay:
-        sys.exit(2)
-
-    tempname = makename((fname1, fname2))
+    exit2onmissing((fname1, fname2))
 
     # TODO: fallbacks (behind a cmdline option) for swapping across filesystems
     # TODO: handle FileExistsError and other exceptions
     # TODO: retry in case tempname is occupied
-    os.rename(fname1, tempname)
-    os.rename(fname2, fname1)
-    os.rename(tempname, fname2)
+    tempname = makename((fname1, fname2))
+    os.rename(fname1, tempname)  # move file 1 to temporary place
+    os.rename(fname2, fname1)  # now move file 2 to now free file 1 place
+    os.rename(tempname, fname2)  # now move original file 1 from temp to file 2 place
 
 
 if __name__ == "__main__":
